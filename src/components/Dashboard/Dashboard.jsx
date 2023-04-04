4b3ccebc88f7a747d0dcabc54b7afbe3d65bba3f
hung202 <artaimer00@gmail.com>
2023-04-04 20:43:38 +0700

    up

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import Navbar from "../Navbar/Navbar";
 import PageContent from "../PageContent/PageContent";
 import { Route, Routes } from "react-router";
 import ViewProfile from "../../pages/MyProfile/ViewProfile/ViewProfile";
+import UpdateProfile from "../../pages/UpdateProfile/UpdateProfile/UpdateProfile";
 import { BrowserRouter } from "react-router-dom";
 
 function Dashboard() {
@@ -19,7 +20,11 @@ function Dashboard() {
                 <Navbar toggle={toggleSidebar} />
 
                 <Routes>
-                    <Route path="users" element={<ViewProfile />} />
+                    <Route path="users" element={<ViewProfile />} >
+                    <Route path="users/update" element={<UpdateProfile />} />
+                    </Route>
+                    {/* <Route path="update" element={<UpdateProfile />} ></Route> */}
+                    
                     {/* <Route path="/device" element={<ViewProfile />}></Route> */}
                     {/* <Route path="/dashboard" element={<Dashboard />} /> */}
                 </Routes>
